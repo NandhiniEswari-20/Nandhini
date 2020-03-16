a6d00bef686b622b6c1843254cfe4dc01adc9023
NandhiniEswari-20 <61401567+NandhiniEswari-20@users.noreply.github.com>
2020-03-16 12:08:48 +0530

    java cloud prgm

---
+++ Pattern.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+
+
+public class Main
+
+{
+	
+public static void main(String[] args) 
+{
+	   
+ int a,b,sum=0;
+	
+	System.out.println("\t\tSUM OF DIGITS");
+	
+	System.out.println("Enter the no. of digits");
+	
+	Scanner s=new Scanner(System.in);
+		
+a=s.nextInt();
+	
+	while(a>0)
+
+		{
+
+		    b=a%10;
+
+		    sum=sum+b;
+	
+	    a=a/10;
+		
+}
+		
+System.out.println("Sum of given digits is:"+sum);
+
+	}
+
+}
